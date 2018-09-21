a0902a59753a3456c1223036edc3d0d7c79cc315
SangYong-Park <31814964+SangYong-Park@users.noreply.github.com>
2018-09-21 09:39:11 +0900

    Update wordCount.py

--- Day4/wordCount.py
+++ Day4/wordCount.py
@@ -1,7 +1,6 @@
 # 단어 검출 개수 출력 => 공백 파싱
 # 포함하지 않는 문자 : 숫자, 특수문자 제외. 
 # 많이 사용된 문자 순으로 sort.
-# 
 
 
 
@@ -13,7 +12,6 @@ f = open('C:\work\day01\Day4\\novel.txt') ## open("읽어올 파일명")
 text = f.read()
 print(len(text))
 
-test = "my Name's sangyong! 'nong' 'sangyong!' 7마리 Names' " # 특수문자는 맨 처음이나 뒤에만 온다는 것.
 words = text.split(' ')
 
 def wordInDic (word, dictionary):
@@ -50,8 +48,3 @@ print("Elapsed time:", str((end-start)))
     
 
 
-
-"""
-1. 단어를 쪼갠다
-2. 딕셔너리에 key-value 값으로 넣는다.
-"""
