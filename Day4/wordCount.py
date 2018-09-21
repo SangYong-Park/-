900e83fa6fe10f1985481da4f877fa060dc511b8
SangYong-Park <31814964+SangYong-Park@users.noreply.github.com>
2018-09-21 09:38:28 +0900

    wordcount 숙제수정
    wordcount 숙제수정

--- Day4/wordCount.py
+++ Day4/wordCount.py
@@ -1,6 +1,9 @@
 # 단어 검출 개수 출력 => 공백 파싱
-# 포함하지 않는 문자 : 숫자, 특수문자 제외. 단 's는 한 단어로 허용.
+# 포함하지 않는 문자 : 숫자, 특수문자 제외. 
 # 많이 사용된 문자 순으로 sort.
+# 
+
+
 
 import sys
 import time
@@ -9,12 +12,40 @@ start = time.time()
 f = open('C:\work\day01\Day4\\novel.txt') ## open("읽어올 파일명")
 text = f.read()
 print(len(text))
-end = time.time()
-print("Elapsed time:", str((end-start)))
 
 test = "my Name's sangyong! 'nong' 'sangyong!' 7마리 Names' " # 특수문자는 맨 처음이나 뒤에만 온다는 것.
-words = test.split(' ')
+words = text.split(' ')
 
+def wordInDic (word, dictionary):
+    if word.isalpha()==False:
+        return
+    if word in dictionary.keys():
+        dictionary[word] = dictionary[word]+1
+    else:
+        dictionary[word] = 1
+
+wordDic = {}
+wordOnlyAlpha = ""
+
+for word in words:
+    if(word.isalpha()==False):
+        for spell in word:
+            if(spell.isalpha()==True):
+                wordOnlyAlpha = wordOnlyAlpha + spell
+            else:
+                continue
+        wordInDic(wordOnlyAlpha.capitalize(), wordDic)
+        wordOnlyAlpha = ""
+    else:
+        wordInDic(word.capitalize(), wordDic)
+
+wordTuple = list(wordDic.items())
+wordTuple.sort(key=lambda element:element[1],reverse=True)
+for wordNum in wordTuple:
+    print(wordNum)
+
+end = time.time()
+print("Elapsed time:", str((end-start)))
 
     
 
